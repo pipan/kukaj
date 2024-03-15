81e4bc53876d987964f1f0822956b13ce7446ddc
Peter Gasparik <gasparik.pe@gmail.com>
2024-03-15 16:53:55 +0100

    upgrade dependency

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -33,7 +33,7 @@ android {
 
 dependencies {
 
-    implementation("androidx.core:core-ktx:1.10.1")
+    implementation("androidx.core:core-ktx:1.12.0")
     implementation("androidx.leanback:leanback:1.0.0")
     implementation("com.github.bumptech.glide:glide:4.11.0")
     implementation("com.android.volley:volley:1.2.1")
