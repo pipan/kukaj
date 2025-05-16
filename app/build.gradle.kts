b3df31421b9c2780a86243301b727088d2f8ad52
Peter Gasparik <gasparik.pe@gmail.com>
2025-05-16 18:55:21 +0200

    v1.0.8

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,8 +11,8 @@ android {
         applicationId = "gaspapp.kukaj"
         minSdk = 26
         targetSdk = 34
-        versionCode = 8
-        versionName = "1.0.7"
+        versionCode = 9
+        versionName = "1.0.8"
 
     }
 
