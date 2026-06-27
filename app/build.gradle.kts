9cf30e1032a35ff0c8e68b1f391add96552e07c2
Peter Gasparik <gasparik.pe@gmail.com>
2026-06-27 22:17:18 +0200

    v1.3.3

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,8 +11,8 @@ android {
         applicationId = "gaspapp.kukaj"
         minSdk = 26
         targetSdk = 35
-        versionCode = 18
-        versionName = "1.3.2"
+        versionCode = 19
+        versionName = "1.3.3"
         vectorDrawables {
             useSupportLibrary = true
         }
